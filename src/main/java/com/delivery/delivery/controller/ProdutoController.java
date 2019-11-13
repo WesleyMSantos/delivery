8a356974227e6162badc940a58895e31cbcb7692
WesleyMSantos <wesleymoura_santos@hotmail.com>
2019-11-13 08:09:23 -0300

    Feito contratos

--- src/main/java/com/delivery/delivery/controller/ProdutoController.java
+++ src/main/java/com/delivery/delivery/controller/ProdutoController.java
@@ -3,20 +3,63 @@ package com.delivery.delivery.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.delivery.delivery.dao.ProdutoDAO;
 import com.delivery.delivery.model.Batata;
+import com.delivery.delivery.model.Bebida;
 import com.delivery.delivery.model.Produto;
+import com.delivery.delivery.model.Tipo;
 
 @Controller
+@RequestMapping("/produtos")
 public class ProdutoController {
 	
 	private static ProdutoDAO dao = new ProdutoDAO();
 
-	@GetMapping("/batatas")
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ResponseBody
+	public Produto inserirProduto(@RequestBody Produto produto) {
+		System.out.println(">>>>>>Inserir produto<<<<<<<");
+		produto = dao.inserir(produto);
+		return produto;
+		 		
+	}
+	
+	@RequestMapping(value = "{id}" , method = RequestMethod.PUT)
+    @ResponseBody
+    Produto atualizarProduto(@PathVariable int id, @RequestBody Produto produto) {
+    	return dao.atualizarProduto(id, produto);
+    }
+	
+	@RequestMapping(value = "/tipos/{tipo}", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Produto> buscarPorTipo(@PathVariable Tipo tipo) {
+		System.out.println(">>>>>>"+tipo);
+		List<Produto> tipos = dao.buscarPorTipo(tipo);
+		
+		return tipos;
+			
+	}
+	
+	
+	//@GetMapping("")
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Produto> buscarTodosProdutos() {
+
+		List<Produto> produtos = dao.buscarTodos();
+		return produtos;
+			
+	}
+	
+	@RequestMapping(value = "/batatas", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Batata> buscarTodasBatatas() {
 
@@ -24,8 +67,17 @@ public class ProdutoController {
 		return batatas;
 			
 	}
+	
+	@RequestMapping(value = "/bebidas", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Bebida> buscarTodasBebidas() {
 
-	@GetMapping("/adicionais")
+		List<Bebida> bebidas = dao.buscarBebidas();
+		return bebidas;
+			
+	}
+
+	@RequestMapping(value = "/adicionais", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Produto> buscarTodosAdicionais() {
 
@@ -33,7 +85,7 @@ public class ProdutoController {
 		return adicionais;
 	}
 	
-	@GetMapping("/batatas/{id}")
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	@ResponseBody
 	Batata buscarUmaBatata(@PathVariable int id) {
 		
@@ -42,11 +94,11 @@ public class ProdutoController {
 
 	}
 	
-	@GetMapping("/batatas/excluir/{id}")
+	//@DeleteMapping("{id}")
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	@ResponseBody
 	public String excluirBatata(@PathVariable int id) {
-		dao.excluirBatata(id);
-		//buscarUmaBatata(id);
+		dao.excluirBatata(id);		
 		return "Batata com id " +id+ " excluída com sucesso";
 	}
 
--- src/main/java/com/delivery/delivery/dao/ProdutoDAO.java
+++ src/main/java/com/delivery/delivery/dao/ProdutoDAO.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.delivery.delivery.model.Batata;
+import com.delivery.delivery.model.Bebida;
 import com.delivery.delivery.model.Produto;
 import com.delivery.delivery.model.Tipo;
 
@@ -11,14 +12,56 @@ public class ProdutoDAO {
 
 	List<Batata> batatas = new ArrayList<Batata>();
 	List<Produto> adicionais = new ArrayList<Produto>();
+	List<Bebida> bebidas = new ArrayList<Bebida>();
 	List<Produto> ingredientes = new ArrayList<Produto>();
-
+	List<Produto> produtos = new ArrayList<Produto>();	
+	
 	public ProdutoDAO() {
 		popularMassaDeTestes();
 	}
+	
+	public Produto inserir(Produto produto) {
+		produtos.add(produto);
+		produto.setId(100);
+		return produto;
+	}
+	
+	public Produto atualizarProduto(int id, Produto produto) {		
+		
+		for (int cont = 0; cont < produtos.size(); cont++) {
+			if (produtos.get(cont).getId() == id) {
+				
+				produtos.set(cont, produto);
+				break;
+			}
+		}
+		
+		return produto;		
+	}
+	
+	public List<Produto> buscarPorTipo(Tipo nome){
+		List<Produto> tiposProdutos = new ArrayList<Produto>();
+		for (Produto tipo : buscarTodos()) {
+			if (tipo.getTipo() == nome) {
+				tiposProdutos.add(tipo);
+			}
+		}
+
+		return tiposProdutos;
+	}	
 
 	public List<Produto> buscarTodosAdicionais() {
 		return adicionais;
+	}
+
+	public List<Produto> buscarTodos() {		
+		return produtos;
+		
+	}
+	
+	public List<Bebida> buscarBebidas() {		
+		return bebidas;
+		
 	}
 
 	public List<Batata> buscarBatatas() {
@@ -37,27 +80,26 @@ public class ProdutoDAO {
 		}
 
 		return encontrou;
-	}	
-	
+	}
+
 	public Batata buscarBatata(int id) {
-		
+
 		for (Batata batata : batatas) {
 			if (batata.getId() == id) {
 				return batata;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	void popularMassaDeTestes() {
 		// POPULANDO LISTA DE ADICIONAIS
 		Produto adicional1 = new Produto();
 		adicional1.setId(1);
 		adicional1.setNome("Bacon");
 		adicional1.setPreco(5.0);
-		adicional1.setQtd(20);
-		adicional1.setObservacao("Sem Observação");
+		adicional1.setQtd(20);		
 		adicional1.setTipo(Tipo.ADICIONAL);
 
 		Produto adicional2 = new Produto();
@@ -65,15 +107,13 @@ public class ProdutoDAO {
 		adicional2.setNome("Calabresa");
 		adicional2.setPreco(1.5);
 		adicional2.setQtd(30);
-		adicional2.setObservacao("Sem Observação");
 		adicional2.setTipo(Tipo.ADICIONAL);
 
 		Produto adicional3 = new Produto();
 		adicional3.setId(3);
 		adicional3.setNome("Creem Cheese");
 		adicional3.setPreco(3.0);
-		adicional3.setQtd(10);
-		adicional3.setObservacao("Sem Observação");
+		adicional3.setQtd(10);	
 		adicional3.setTipo(Tipo.ADICIONAL);
 
 		adicionais.add(adicional1);
@@ -100,7 +140,7 @@ public class ProdutoDAO {
 		batata1.setNome("Batata1");
 		batata1.setPreco(5.0);
 		batata1.setQtd(20);
-		batata1.setObservacao("Sem Observação");
+		batata1.setObservacao("Gratinada");
 		batata1.setTipo(Tipo.BATATA);
 		batata1.setAdicionais(buscarTodosAdicionais());
 		batata1.setIngredientes(buscarTodosAdicionais());
@@ -110,7 +150,7 @@ public class ProdutoDAO {
 		batata2.setNome("Batata2");
 		batata2.setPreco(1.5);
 		batata2.setQtd(30);
-		batata2.setObservacao("Sem Observação");
+		batata2.setObservacao("Frita");
 		batata2.setTipo(Tipo.BATATA);
 		batata2.setAdicionais(buscarTodosAdicionais());
 		batata2.setIngredientes(buscarTodosAdicionais());
@@ -120,7 +160,7 @@ public class ProdutoDAO {
 		batata3.setNome("Batata3");
 		batata3.setPreco(3.0);
 		batata3.setQtd(10);
-		batata3.setObservacao("Sem Observação");
+		batata3.setObservacao("Assada");
 		batata3.setTipo(Tipo.BATATA);
 		batata3.setAdicionais(buscarTodosAdicionais());
 		batata3.setIngredientes(buscarTodosAdicionais());
@@ -128,6 +168,27 @@ public class ProdutoDAO {
 		batatas.add(batata1);
 		batatas.add(batata2);
 		batatas.add(batata3);
+		
+		//POPULANDO AS BEBIDAS
+        Bebida bebida1 = new Bebida();
+        bebida1.setNome("Fanta");
+        bebida1.setTipo(Tipo.BEBIDA);
+        
+        Bebida bebida2 = new Bebida();
+        bebida2.setNome("Coca-Cola");
+        bebida2.setTipo(Tipo.BEBIDA);
+        
+        Bebida bebida3 = new Bebida();
+        bebida3.setNome("Suco Laranja");
+        bebida3.setTipo(Tipo.BEBIDA);
+        
+        bebidas.add(bebida1);
+        bebidas.add(bebida2);
+        
+        produtos.addAll(batatas);
+        produtos.addAll(adicionais);
+        produtos.addAll(bebidas);
 
 	}
+
 }
